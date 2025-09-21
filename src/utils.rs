1bf74ebbd2fc42956709946e3fca969acd353b4a
Luki1235512 <luki1235512@gmail.com>
2025-09-21 22:21:26 +0200

    Create a new file only when it doesn't exist

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,14 @@
 use std::{env, fs};
 
 pub fn create_test_file() -> Result<(), Box<dyn std::error::Error>> {
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let some_dir = current_dir.join("some").join("directory");
+    let test_file_path = some_dir.join("example1-simple.html");
+
+    if test_file_path.exists() {
+        return Ok(());
+    }
+
     let test_content = r#"<html>
     <body>
         <div>This is a simple</div>
@@ -9,13 +17,8 @@ pub fn create_test_file() -> Result<(), Box<dyn std::error::Error>> {
     </body>
 </html>"#;
 
-    let current_dir = env::current_dir().expect("Failed to get current directory");
-    let some_dir = current_dir.join("some").join("directory");
-    let test_file_path = some_dir.join("example1-simple.html");
-
     fs::create_dir_all(&some_dir)?;
 
     fs::write(&test_file_path, test_content)?;
-    println!("Created test file at: {}", test_file_path.display());
     Ok(())
 }
